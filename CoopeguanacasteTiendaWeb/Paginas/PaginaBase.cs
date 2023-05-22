c0123d408ec47046c7fd35c4319dba7778bf14c8
moragachris <christian.moraga@cwc.com>
2023-05-21 23:15:21 -0600

    Cambios en estructura y forma

---
+++ CoopeguanacasteTiendaWeb/Paginas/PaginaBase.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoopeguanacasteTiendaWeb.Paginas
+{
+    internal class PaginaBase
+    {
+        private IWebDriver driver;
+        protected String urlCoopeguanaste = "https://tienda.coopeguanacaste.com/";
+        private WebDriverWait espera;
+
+        public PaginaBase(IWebDriver driver)
+        {
+            this.driver = driver;
+            espera = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            driver.Manage().Window.Maximize();
+        }
+        public void goToPage(string ext = null)
+        {
+            driver.Navigate().GoToUrl(urlCoopeguanaste+ext);
+        }
+
+        public void BuscarElemento(By elemento)
+        {
+            driver.FindElement(elemento);
+        }
+
+        public void BuscarElementos(By elemento)
+        {
+            driver.FindElements(elemento);
+        }
+
+        public void EsperaTituloContenga(String texto) 
+        {
+            espera.Until(ExpectedConditions.TitleContains(texto));
+        }
+
+        public void EsperaASerCliclado(By elemento)
+        {
+            espera.Until(ExpectedConditions.ElementToBeClickable(elemento));
+        }
+
+        public void EsperaElementoVisible(By elemento)
+        {
+            espera.Until(ExpectedConditions.ElementIsVisible(elemento));
+        }
+
+        public String getPageTitle()
+        {
+            return driver.Title;
+        }
+    }
+}
--- CoopeguanacasteTiendaWeb/Pruebas/ComprasTiendaPruebas.cs
+++ CoopeguanacasteTiendaWeb/Pruebas/ComprasTiendaPruebas.cs
@@ -1,3 +1,4 @@
+using CoopeguanacasteTiendaWeb.Paginas;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -5,6 +6,7 @@ using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using System;
 using System.Linq;
+using System.Threading;
 
 namespace CoopeguanacasteTiendaWeb
 {
@@ -14,7 +16,8 @@ namespace CoopeguanacasteTiendaWeb
 
         private ChromeDriver driver;
         private static string baseURL;
-        private WebDriverWait espera;
+        //private WebDriverWait espera;
+        private PaginaBase paginaBase;
         //private String currentURL;
 
         [TestInitialize]
@@ -23,49 +26,51 @@ namespace CoopeguanacasteTiendaWeb
             // Initialize
             ChromeOptions options = new ChromeOptions
             {
-                PageLoadStrategy = PageLoadStrategy.Normal              
+                PageLoadStrategy = PageLoadStrategy.Normal
             };
             options.AddArgument(@"--incognito");
             driver = new ChromeDriver(options);
-            baseURL = "https://tienda.coopeguanacaste.com/";
-            driver.Url = baseURL;
-            espera = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
-            driver.Manage().Window.Maximize();
-
+            //baseURL = "https://tienda.coopeguanacaste.com/";
+            // driver.Url = baseURL;
+            //espera = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
+            //driver.Manage().Window.Maximize();
+            paginaBase = new PaginaBase(driver);
+            paginaBase.goToPage();
         }
         public void InicioSesion()
         {
             driver.FindElement(By.XPath("//div[@id='header-links-opener']/span")).Click();
-            driver.Navigate().GoToUrl($"{baseURL}extend/login");
-            //TimeSpan.FromSeconds(2);
+            paginaBase.goToPage("extend/login");
+            //driver.Navigate().GoToUrl($"{paginaBase.goToPage("extend/login")}extend/login");
+            Thread.Sleep(2);   
             driver.FindElement(By.Id("Username")).Click();
             driver.FindElement(By.Id("Username")).Clear();
             driver.FindElement(By.Id("Username")).SendKeys("504010486");
-            //TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             driver.FindElement(By.Id("Password")).Click();
             driver.FindElement(By.Id("Password")).Clear();
             driver.FindElement(By.Id("Password")).SendKeys("Cg12.mm94");
-            //TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             driver.FindElement(By.CssSelector(".login-button")).Click();
-            TimeSpan.FromSeconds(2);
-            driver.Navigate().GoToUrl(baseURL);
-            espera.Until(ExpectedConditions.TitleContains("Tienda Coopeguanacaste"));
+            Thread.Sleep(2);
+            paginaBase.goToPage();
+            paginaBase.EsperaTituloContenga("Tienda Coopeguanacaste");
         }
 
         public void CerrarSesion()
         {
             driver.FindElement(By.XPath("//div[@id='header-links-opener']/span")).Click();
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             driver.FindElement(By.XPath("//a[contains(text(),'Cerrar sesión')]")).Click();
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             driver.Navigate().GoToUrl(baseURL);
         }
 
         [TestMethod]
         public void VerificarTituloPagina()
         {
-            TimeSpan.FromSeconds(2);
-            Assert.AreEqual("Tienda Coopeguanacaste", driver.Title);
+            Thread.Sleep(2);   
+            Assert.AreEqual("Tienda Coopeguanacaste", paginaBase.getPageTitle());
         }
 
         [TestMethod]
@@ -75,26 +80,26 @@ namespace CoopeguanacasteTiendaWeb
             /*
             driver.FindElement(By.XPath("//div[@id='header-links-opener']/span")).Click();
             driver.Navigate().GoToUrl($"{baseURL}extend/login");
-            //TimeSpan.FromSeconds(2);
+            //Thread.Sleep(2);   
             driver.FindElement(By.Id("Username")).Click();
             driver.FindElement(By.Id("Username")).Clear();
             driver.FindElement(By.Id("Username")).SendKeys("504010486");
-            //TimeSpan.FromSeconds(2);
+            //Thread.Sleep(2);   
             driver.FindElement(By.Id("Password")).Click();
             driver.FindElement(By.Id("Password")).Clear();
             driver.FindElement(By.Id("Password")).SendKeys("Cg12.mm94");
-            //TimeSpan.FromSeconds(2);
+            //Thread.Sleep(2);   
             driver.FindElement(By.CssSelector(".login-button")).Click();
             driver.Navigate().GoToUrl(baseURL);
             */
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             driver.FindElement(By.XPath("//a[contains(text(),'Carrito de compras')]")).Click();
-            TimeSpan.FromSeconds(2);
-            espera.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("button.button-1.cart-button > span")));
+            Thread.Sleep(2);
+            paginaBase.EsperaASerCliclado(By.CssSelector("button.button-1.cart-button > span"));
             driver.FindElement(By.CssSelector("button.button-1.cart-button > span")).Click();
             driver.FindElement(By.XPath("//form[@id='shopping-cart-form']/div[2]/div/button")).Click();
-            TimeSpan.FromSeconds(2);
-            Assert.AreEqual("Carrito de compras. Tienda Coopeguanacaste", driver.Title);
+            Thread.Sleep(2);   
+            Assert.AreEqual("Carrito de compras. Tienda Coopeguanacaste", paginaBase.getPageTitle());
             //CerrarSesion();
         }
 
@@ -103,11 +108,11 @@ namespace CoopeguanacasteTiendaWeb
         {
             InicioSesion();
 
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
 
             driver.FindElement(By.CssSelector("div.wishlist-button > a.ico-wishlist")).Click();
 
-            Assert.AreEqual("Lista de Deseos. Tienda Coopeguanacaste", driver.Title);
+            Assert.AreEqual("Lista de Deseos. Tienda Coopeguanacaste", paginaBase.getPageTitle());
 
             //CerrarSesion();
         }
@@ -115,18 +120,20 @@ namespace CoopeguanacasteTiendaWeb
         [TestMethod]
         public void BusquedaPorCategoria()
         {
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             driver.FindElement(By.Id("instant-search-categories")).Click();
+
             new SelectElement(driver.FindElement(By.Id("instant-search-categories"))).SelectByText("Tecnología");
             driver.FindElement(By.Id("small-searchterms")).Click();
             driver.FindElement(By.Id("small-searchterms")).Clear();
             driver.FindElement(By.Id("small-searchterms")).SendKeys("Celulares");
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);  
+            
             driver.FindElement(By.XPath("//button[@type='submit']")).Click();
 
-            espera.Until(ExpectedConditions.TitleContains(driver.Title));
+            paginaBase.EsperaTituloContenga("Búsqueda. Tienda Coopeguanacaste");
 
-            Assert.AreEqual("Búsqueda. Tienda Coopeguanacaste", driver.Title);
+            Assert.AreEqual("Búsqueda. Tienda Coopeguanacaste", paginaBase.getPageTitle());
         }
 
         [TestMethod]
@@ -136,32 +143,33 @@ namespace CoopeguanacasteTiendaWeb
             TimeSpan.FromSeconds(3);
             //espera.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//li[2]/span")));
             //driver.FindElement(By.XPath("//li[2]/span")).Click(); 
-            //TimeSpan.FromSeconds(2);
+            //Thread.Sleep(2);   
             /*
             driver.FindElement(By.CssSelector("a[title='Mostrar productos en la categoría Tecnología'] > img.lazy")).Click();
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Añadir a la lista de comparación'])[6]/following::button[1]")).Click();
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Cantidad : 1'])[1]/following::button[1]")).Click();
             */
             
             //driver.FindElement(By.XPath("//a[@href='/tecnologia']")).Click();
-            //TimeSpan.FromSeconds(2);
+            //Thread.Sleep(2);   
             //driver.FindElement(By.XPath("//button[@class='button-2 add-to-wishlist-button']")).Click();
             //TimeSpan.FromSeconds(3);
             //driver.FindElement(By.CssSelector(".item-box:nth-child(6) .add-to-wishlist-button")).Click();
-            //TimeSpan.FromSeconds(2);
+            //Thread.Sleep(2);   
             //espera.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(".productAddedToCartWindowCheckout")));
             //driver.FindElement(By.CssSelector(".productAddedToCartWindowCheckout")).Click();
             
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);
             //espera.Until(ExpectedConditions.ElementToBeClickable(By.Name("updatecart")));
             //driver.FindElement(By.Name("updatecart")).Click();
-            //Assert.AreEqual("Tienda Coopeguanacaste", driver.Title);
-            espera.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("div.wishlist-button > a.ico-wishlist")));
+            //Assert.AreEqual("Tienda Coopeguanacaste", paginaBase.getPageTitle());
+            paginaBase.EsperaASerCliclado(By.CssSelector("div.wishlist-button > a.ico-wishlist"));
+            //espera.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("div.wishlist-button > a.ico-wishlist")));
             driver.FindElement(By.CssSelector("div.wishlist-button > a.ico-wishlist")).Click();
 
-            Assert.AreEqual("Lista de Deseos. Tienda Coopeguanacaste", driver.Title);
+            Assert.AreEqual("Lista de Deseos. Tienda Coopeguanacaste", paginaBase.getPageTitle());
             //CerrarSesion();
         }
 
@@ -177,10 +185,10 @@ namespace CoopeguanacasteTiendaWeb
             }
             driver.FindElement(By.Id("IDNumber")).Click();
             driver.FindElement(By.Id("IDNumber")).SendKeys("5-04010486");
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             driver.FindElement(By.CssSelector(".male > .forcheckbox")).Click();
             //driver.FindElement(By.Id("FirstName")).Click();
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             driver.FindElement(By.Name("DateOfBirthDay")).Click();
             {
                 IWebElement dropdownDay = driver.FindElement(By.Name("DateOfBirthDay"));
@@ -196,24 +204,25 @@ namespace CoopeguanacasteTiendaWeb
                 IWebElement dropdownYear = driver.FindElement(By.Name("DateOfBirthYear"));
                 dropdownYear.FindElement(By.XPath("//option[. = '1994']")).Click();
             }
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             driver.FindElement(By.Id("Email")).Click();
             driver.FindElement(By.Id("Email")).SendKeys("christiangeo94@hotmail.com");
             driver.FindElement(By.Id("Password")).Click();
             driver.FindElement(By.Id("Password")).SendKeys("Cg12.mm94");
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             driver.FindElement(By.Id("ConfirmPassword")).Click();
             driver.FindElement(By.Id("ConfirmPassword")).SendKeys("Cg12.mm94");
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             driver.FindElement(By.CssSelector(".k-dropdown-wrap > .k-input")).Click();
             driver.FindElement(By.CssSelector(".k-state-hover")).Click();
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             //driver.FindElement(By.Id("StreetAddress")).Click();
             //driver.FindElement(By.Id("StreetAddress")).SendKeys("Barrio San Isidro frente al parque los Marañones");
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             //driver.FindElement(By.Id("register-button")).Click();
-            TimeSpan.FromSeconds(2);
-            espera.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[contains(@id,'dialog-notifications-error')]")));
+            Thread.Sleep(2);
+            paginaBase.EsperaElementoVisible(By.XPath("//*[contains(@id,'dialog-notifications-error')]"));
+            //espera.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[contains(@id,'dialog-notifications-error')]")));
             String mensajeError = driver.FindElement(By.XPath("//*[contains(@id,'dialog-notifications-error')]")).Text;
 
             Assert.AreEqual("Ya existe un cliente registrado para este número de cédula", mensajeError);
@@ -222,36 +231,39 @@ namespace CoopeguanacasteTiendaWeb
         [TestMethod]
         public void IngresoACooopeguanacasteTienda()
         {
+            //Arrange
             driver.FindElement(By.XPath("//div[@id='header-links-opener']/span")).Click();
-            driver.Navigate().GoToUrl($"{baseURL}extend/login");
-            //TimeSpan.FromSeconds(2);
+            paginaBase.goToPage("extend/login");
+
+            //ACT
+            Thread.Sleep(2);   
             //driver.FindElement(By.Id("Username")).Click();
             driver.FindElement(By.Id("Username")).Clear();
             driver.FindElement(By.Id("Username")).SendKeys("504010486");
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             //driver.FindElement(By.Id("Password")).Click();
             driver.FindElement(By.Id("Password")).Clear();
             driver.FindElement(By.Id("Password")).SendKeys("Cg12.mm94");
-            //TimeSpan.FromSeconds(2);
+            //Thread.Sleep(2);   
             driver.FindElement(By.CssSelector(".login-button")).Click();
             TimeSpan.FromSeconds(4);
-            driver.Navigate().GoToUrl(baseURL);
-            espera.Until(ExpectedConditions.TitleContains("Tienda Coopeguanacaste"));
+            paginaBase.goToPage();
+            paginaBase.EsperaTituloContenga("Tienda Coopeguanacaste");
             TimeSpan.FromSeconds(5);
-
-            Assert.AreEqual("Tienda Coopeguanacaste", driver.Title);
+            //Assert
+            Assert.AreEqual("Tienda Coopeguanacaste", paginaBase.getPageTitle());
             /*
             driver.FindElement(By.XPath("//div[@id='header-links-opener']/span")).Click();
             driver.FindElement(By.CssSelector(".ico-account")).Click();
             var newWindow = driver.SwitchTo().Window(newWindowHandle);
             newWindow.WindowHandles.FirstOrDefault();
             newWindow.Navigate().GoToUrl($"{baseURL}Plugins/EcommerceCostaRica/CustomerExtend/Info");
-            TimeSpan.FromSeconds(2);
+            Thread.Sleep(2);   
             newWindow.FindElement(By.CssSelector("#header-links-opener > span")).Click();
             newWindow.FindElement(By.CssSelector(".ico-logout")).Click();
             */
             //driver.FindElement(By.CssSelector("#header-links-opener > span")).Click();
-            //TimeSpan.FromSeconds(2);
+            //Thread.Sleep(2);   
             //var newWindow = driver.SwitchTo().Window(newWindowHandle);
             //newWindow.WindowHandles.FirstOrDefault();
             //driver.FindElement(By.CssSelector(".ico-account")).Click();
@@ -261,7 +273,7 @@ namespace CoopeguanacasteTiendaWeb
 
             //espera.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[contains(text(),'Mi cuenta')]")));
             //driver.FindElement(By.XPath("//a[contains(text(),'Mi cuenta')]")).Click();
-            //TimeSpan.FromSeconds(2);
+            //Thread.Sleep(2);   
             ////driver.Navigate().GoToUrl($"{baseURL}Plugins/EcommerceCostaRica/CustomerExtend/Info");
             //driver.FindElement(By.Id("save-info-button")).Click();
             //driver.FindElement(By.XPath("//div[@id='header-links-opener']/span")).Click();
